b9dc8d253e899cad0169fa10018a891a2ea8750a
Nakudo <guilhermetakashi@admake.com.br>
2017-12-28 16:28:27 -0200

    statament to not need for define env variable

--- configs/db.js
+++ configs/db.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose');
 const Bluebird = require('bluebird');
 mongoose.Promise  = Bluebird;
 console.log(process.env.DATABASEURL);
-module.exports = mongoose.connect(process.env.DATABASEURL,{
+module.exports = mongoose.connect(process.env.DATABASEURL||'mongodb://localhost/camps',{
     useMongoClient:true
 });
