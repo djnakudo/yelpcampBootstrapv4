92e04059fa6b55990ec55e4bf6cbe87057fc01af
Nakudo <guilhermetakashi@admake.com.br>
2017-12-28 16:22:27 -0200

    db for local or heroku deployment

--- configs/db.js
+++ configs/db.js
@@ -1,11 +1,7 @@
 const mongoose = require('mongoose');
 const Bluebird = require('bluebird');
 mongoose.Promise  = Bluebird;
-//local deploy
-//const url = 'mongodb://localhost/camps';
-const url = 'mongodb://nakajima:bing12@ds143330.mlab.com:43330/yelpcampnaks';
-// local deployment : url mongodb://localhost/camps
-//mongodb://nakajima:bing12@ds143330.mlab.com:43330/yelpcampnaks
-module.exports = mongoose.connect('mongodb://nakajima:bing12@ds143330.mlab.com:43330/yelpcampnaks',{
+console.log(process.env.DATABASEURL);
+module.exports = mongoose.connect(process.env.DATABASEURL,{
     useMongoClient:true
 });
